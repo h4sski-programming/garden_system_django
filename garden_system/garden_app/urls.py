3233e349a0ab1f8ce3de1a75b56ff694d08026eb
h4sski <h4sski.programming.gmail.com>
2023-06-16 14:17:52 +0200

    done btn change status in db

--- garden_system/garden_app/urls.py
+++ garden_system/garden_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name = 'garden_app'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('zone/<int:id>', views.zone_settings, name='zone_settings'),
+    path('change_state/<int:id>', views.change_zone_state, name='change_zone_state'),
 ]
--- garden_system/garden_app/views.py
+++ garden_system/garden_app/views.py
@@ -1,16 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+
+from .models import Zone
 # Create your views here.
 
 def index(request):
     
     context = {
-        'zones': {
-            'zone_1': True,
-            'zone_2': True,
-            'zone_3': False,
-            'zone_4': True,
-        },
-        'temp': 'temp'
+        'zones': Zone.objects.all(),
+        'temp': 'temp',
     }
     return render(request=request, template_name='index.html', context=context)
+
+
+def zone_settings(request, id):
+    
+    context = {
+        'zone': Zone.objects.get(pk=id),
+        'temp': 'temp'
+    }
+    return render(request=request, template_name='zone_settings.html', context=context)
+
+
+
+def change_zone_state(request, id):
+    zone = Zone.objects.get(pk=id)
+    print(zone.state)
+    if zone.state:
+        zone.state = False
+    else:
+        zone.state = True
+    print(zone.state)
+    zone.save()
+        
+    return redirect('/')
